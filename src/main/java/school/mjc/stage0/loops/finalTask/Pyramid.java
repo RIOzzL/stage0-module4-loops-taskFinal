402c198e7bfd60440e7f8944046cd52b092d6f11
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-07-02 20:53:08 +0300

    "Module4 - loops Final Task"

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) { // елка из цифр
+            for (int k = cathetusLength; k > i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
